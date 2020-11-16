4fbcad88202651240344b115e254923cdac28e88
Tejas Patel <tejas.patel@dal.ca>
2020-11-16 16:57:19 -0400

    server.js without /dist

--- server.js
+++ server.js
@@ -3,11 +3,11 @@ const path = require('path');
  
 const app = express();
  
-app.use(express.static(__dirname + '/dist/test-cicd-heroku-app'));
+app.use(express.static(__dirname + '/test-cicd-heroku-app'));
  
 app.get('/*', function(req,res) {
  
-res.sendFile(path.join(__dirname+'/dist/test-cicd-heroku-app/index.html'));
+res.sendFile(path.join(__dirname+'/test-cicd-heroku-app/index.html'));
 });
  
 app.listen(process.env.PORT || 8080);
